0b0e8504b42ef926802b5cd1ee90e7084402c98e
Marty Zalega <evilmarty@gmail.com>
2012-07-22 20:41:42 +1000

    a nicer check for core extensions

--- lib/scrape/core_ext/array.rb
+++ lib/scrape/core_ext/array.rb
@@ -1,5 +1,5 @@
 class Array
   def extract_options!
     last.instance_of?(Hash) ? pop : {}
-  end unless instance_methods.include?(:extract_options!)
+  end unless Array.respond_to?(:extract_options!)
 end
--- lib/scrape/core_ext/string.rb
+++ lib/scrape/core_ext/string.rb
@@ -2,5 +2,5 @@ class String
   def starts_with str
     str = str.to_str
     self[0, str.length] == str
-  end unless instance_methods.include?(:starts_with)
+  end unless String.respond_to?(:starts_with)
 end
